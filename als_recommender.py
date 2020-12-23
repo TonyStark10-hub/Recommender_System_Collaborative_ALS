401ffaddb5ab1e264a343cad949e7f45761491d2
Tanmay Solanki <tony@MacBook-Pro-3.lan>
2020-12-23 15:51:37 -0400

    created dfs and split it in training and validation dfs

--- als_recommender.py
+++ als_recommender.py
@@ -11,3 +11,28 @@ import numpy as np
 from IPython.display import Image
 from IPython.display import display
 
+#creation of spark context and spark session
+
+spark=ps.sql.SparkSession.builder.Master('local[*]').appName('ALS_Recommender').getOrCreate()
+
+sc=spark.SparkContext()
+sqlContext=SQLContext(sc)
+
+#Creating Dataframe for ratings of each books
+
+ratings_df=spark.read.csv('RowData/ratings.csv',header=True,inferSchema=True)
+#rating_df.printSchema()
+#rating_df.show(5)
+
+#Creating Dataframe for book dataset
+books_df=spark.read.csv('RawData/books.csv',header=True,inferSchema=True)
+#bookss_df.printSchema()
+#books_df.show(1)
+
+#partition of rating_df in training and validation df
+
+training_df,validation_df=ratings_df.randomSplit([.8,.2])
+ 
+
+
+
